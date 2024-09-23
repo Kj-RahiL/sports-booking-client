f0eba585aa0685960e8ad5c147167d4d5dd55a05
Kj-RahiL <rahiilarham@gmail.com>
2024-09-23 16:37:42 +0600

    add map

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,19 +1,19 @@
-
-
 const Contact = () => {
-    return (
-      <div className="bg-gray-100 py-20">
-          <div className="flex justify-center items-start container mx-auto mt-10 ">
+  return (
+    <div className="bg-gray-100 py-20">
+      <div className="flex justify-center items-start container mx-auto mt-10 ">
         {/* Left Section */}
         <div className="w-1/2 pr-8">
-          <h2 className="text-2xl font-bold mb-4">We are here to help!</h2>
-          <p className="text-gray-700 mb-4">
-            Let us know how we can best serve you. Use the contact form to email us
-            or select from the topics below that best fit your needs. It's an honor
-            to support you in your journey towards better health.
-          </p>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47565.84672074542!2d91.06079009877054!3d22.854753643238233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3754a58103900e93%3A0xb7ae99632ba501f1!2z4Kao4KeL4Kef4Ka-4KaW4Ka-4Kay4KeA!5e1!3m2!1sbn!2sbd!4v1727087671999!5m2!1sbn!2sbd"
+            width="600"
+            height="450"
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
-  
+
         {/* Right Section: Form */}
         <div className="w-1/2">
           <form className="space-y-4">
@@ -44,15 +44,22 @@ const Contact = () => {
             </button>
           </form>
           <p className="text-xs text-gray-500 mt-4">
-            This site is protected by reCAPTCHA and the Google 
-            <a href="#" className="underline"> Privacy Policy </a> and 
-            <a href="#" className="underline"> Terms of Service </a> apply.
+            This site is protected by reCAPTCHA and the Google
+            <a href="#" className="underline">
+              {" "}
+              Privacy Policy{" "}
+            </a>{" "}
+            and
+            <a href="#" className="underline">
+              {" "}
+              Terms of Service{" "}
+            </a>{" "}
+            apply.
           </p>
         </div>
       </div>
-      </div>
-    );
-  };
-  
-  export default Contact;
-  
+    </div>
+  );
+};
+
+export default Contact;
